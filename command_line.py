f9af959a19b397c9e173c26fc334fd3e0d0760b3
Renan Biegelmeyer <renan.biegel@gmail.com>
2020-07-28 02:05:04 -0300

    Improved command line.

--- command_line.py
+++ command_line.py
@@ -1,10 +1,29 @@
-from routes.services.shortest_path_service import ShortestPathService
-from routes.services.utils import load_csv_into_graph
+import os
+import sys
+
+from services.shortest_path_service import ShortestPathService
+from services.utils import load_csv_into_graph
 
 if __name__ == '__main__':
-    graph = load_csv_into_graph('./input-file.txt')
-    start = 'GRU'
-    end = 'CDG'
+
+    if len(sys.argv) != 2:
+        print(f'You need to provide a input file. \n\nEx.: python {sys.argv[0]} my-file.csv')
+        exit(-1)
+
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print(f'Couldn\'t find {filename}')
+        exit(-1)
+    graph = load_csv_into_graph(filename)
+
+    try:
+        entered_input = input('please enter the route: ')
+        splitted = entered_input.split('-')
+        start = splitted[0].upper()
+        end = splitted[1].upper()
+    except IndexError:
+        print('You need to enter the route in the following format: START-END')
+        exit(-1)
 
     sps = ShortestPathService(graph, start, end)
 
